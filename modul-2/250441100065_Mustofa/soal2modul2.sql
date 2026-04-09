05e7c8cbcdc82297156776fbd219958eaec460f7
initofa <mus2108000@gmail.com>
2026-04-09 21:34:58 +0700

    250441100065_Mustofa_Modul2_AsprakKakFaisal

---
+++ modul-2/250441100065_Mustofa/soal2modul2.sql
@@ -0,0 +1,14 @@
+INSERT INTO dokter VALUES
+(1,'Dr. tofa','Umum'),
+(2,'Dr. adit','Gigi');
+
+INSERT INTO pasien VALUES
+(1,'Budi','2000-05-10','081234567890'),
+(2,'wildan','1998-09-21','082345678901');
+
+INSERT INTO rekam_medis VALUES
+(1,1,1,'2026-04-01','Demam'),
+(2,2,2,'2026-04-02','Sakit gigi');
+
+INSERT INTO rekam_medis VALUES
+(3,1,99,'2026-04-03','Flu');
---
+++ modul-2/250441100065_Mustofa/soal3modul2.sql
@@ -0,0 +1,12 @@
+UPDATE pasien
+SET no_telepon = '081873274523'
+WHERE id_pasien = 1;
+
+UPDATE dokter
+SET spesialisasi = 'Penyakit Dalam'
+WHERE id_dokter = 1;
+
+DELETE FROM rekam_medis
+WHERE id_rekam = 2;
+
+SELECT * FROM rekam_medis;
